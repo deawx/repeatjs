599a626cc97c5bc33c9666d86009278991406a7c
Leonhard Pfob <leo.pfob@outlook.de>
2018-10-07 13:58:08 +0200

    Fixed order of repeation and add text repeation support

--- src/repeat.js
+++ src/repeat.js
@@ -5,7 +5,7 @@ repeat.getZIndex = function(element)
     var index = 0;
     var parent = element.parentElement;
 
-    while(parent.tagName != 'BODY' && index < 10) 
+    while(parent.tagName != 'BODY' && parent && index < 100) 
     {
         index++;
         parent = parent.parentElement;
@@ -19,7 +19,35 @@ repeat.compareIndexedElements = function(a, b)
     return b.index - a.index;
 }
 
-repeat.initialize = function()
+repeat.initializeTexts = function()
+{
+    var elements = document.querySelectorAll('[repeat-text]');
+    var indexedElements = [];
+
+    for (const e of elements) 
+    {
+        indexedElements.push(
+        {
+            element: e,
+            index: repeat.getZIndex(e)
+        });
+    }
+
+    indexedElements.sort(repeat.compareIndexedElements);
+
+    for (const indexedElement of indexedElements) 
+    {
+        let text = indexedElement.element.innerHTML;
+        let repeat = indexedElement.element.getAttribute("repeat-text") - 1;
+        indexedElement.element.removeAttribute("repeat-text");
+
+        for (var i = 0; i < repeat; i++)
+        {
+            indexedElement.element.innerHTML += text;
+        }
+    }
+}
+repeat.initializeElements = function()
 {
     var elements = document.querySelectorAll('[repeat-element]');
     var indexedElements = [];
@@ -40,11 +68,27 @@ repeat.initialize = function()
         let repeat = indexedElement.element.getAttribute("repeat-element") - 1;
         indexedElement.element.removeAttribute("repeat-element");
 
-        for (var i = 0; i < repeat; i++)
+        if (indexedElement.element.parentElement.childElementCount > 1)
         {
-            indexedElement.element.parentElement.appendChild(indexedElement.element.cloneNode(true));
+            for (var i = 0; i < repeat; i++)
+            {
+                indexedElement.element.parentElement.insertBefore(indexedElement.element.cloneNode(true), indexedElement.element);
+            }
+        }
+        else
+        {
+            for (var i = 0; i < repeat; i++)
+            {
+                indexedElement.element.parentElement.appendChild(indexedElement.element.cloneNode(true));
+            }
         }
     }
+}
+
+repeat.initialize = function()
+{
+    repeat.initializeTexts();
+    repeat.initializeElements();
 }
 
 window.addEventListener("load", repeat.initialize);
